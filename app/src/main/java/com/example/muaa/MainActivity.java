e01187b39545c2603fd1162cd74625dbe324df89
João Marcos <jmorodrigues@dcomp.ufs.br>
2020-12-05 21:33:44 -0300

    Implementando splash screen.

--- app/src/main/java/com/example/muaa/MainActivity.java
+++ app/src/main/java/com/example/muaa/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.muaa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
@@ -22,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         titulo = findViewById(R.id.txt_titulo);
         ouvir = findViewById(R.id.btn_ouvir);
         progressBar = findViewById(R.id.progress_bar);
+
+        ouvir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                titulo.setText("Ouvindo...");
+                // Trocar a imagem por outra
+            }
+        });
     }
 }
